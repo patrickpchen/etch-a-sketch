20aa2a052d5378724f297188ffc8b23064766a8a
Pin-Hsi Patrick Chen <patrick.tucson@gmail.com>
2022-05-10 00:05:35 +0800

    Add a limit of 100 to the function askAndCreate

--- script.js
+++ script.js
@@ -7,39 +7,37 @@ resetButton.addEventListener('click', askAndCreate);
 
 function askAndCreate(){
     let numOfPix = prompt('How many pixels per side do you want? (Keep it under 100.)', '16');
-    numOfPix = Number(numOfPix);
-
-    //Remove the existing grid first.
-    while(containerGrid.hasChildNodes()){
-        containerGrid.removeChild(containerGrid.firstChild);
+    if (numOfPix < 0 || numOfPix > 100){
+        alert ("Please provide a value between 1 and 100.");
+    } else {
+        numOfPix = Number(numOfPix);
+        //Remove the existing grid first.
+        while(containerGrid.hasChildNodes()){
+            containerGrid.removeChild(containerGrid.firstChild);
+        }
+        //Create the new grid.
+        createGrid(numOfPix);
     }
-    //Create the new grid.
-    createGrid(numOfPix);
 }
 
 //Create divs for the grid.
-
 function createGrid(x){
     for (let i = 0; i < x; i++){
-
         //Create X number of rows.
         let littleGrid = document.createElement('div');
         littleGrid.setAttribute('style', 'display: flex; flex: 1 1 auto;');
 
         for (let i = 0; i < x; i++){
-            //Create X number of little divs and put them in a row.
-        
+            //Create X number of little divs and put them in a row.   
             let littleDiv = document.createElement('div');
             littleDiv.setAttribute('style','border-color: black; border: 1px solid; flex: 1 1 auto;');
             littleDiv.classList.add('pixel');
-            //Put the divs in the container.
-        
+            //Put the divs in the container.   
             littleGrid.appendChild(littleDiv);
         }
         containerGrid.appendChild(littleGrid);
     }
     let pix = document.querySelectorAll('.pixel');
-
     //Add an event listener to every pixel.
     pix.forEach((p) => {
         p.addEventListener('mouseover', changeColor);
